636c35a178738b5bc49fa20927a4c83a9b8b2e6e
kategladyo <kategladyo@yandex.ru>
2018-10-15 11:27:47 +0300

    Прогнана caesar.py через pep8

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -9,23 +9,22 @@ def encrypt_caesar(plaintext):
     >>> encrypt_caesar("")
     ''
     """
-    # PUT YOUR CODE HERE
-    word=[]
+    # my code
+    word = []
     for i in plaintext:
-        i=ord(i)
-        if i>119 and i<123:
-            i=i-23
-        elif i>96 and i<120:
-            i=i+3
-        elif i>87 and i<91:
-            i=i-23
-        elif i>64 and i<88:
-            i=i+3
-        i=chr(i)
-        word.append(i)   
-    ciphertext=(''.join(word))
+        i = ord(i)
+        if i > 119 and i < 123:
+            i = i - 23
+        elif i > 96 and i < 120:
+            i = i + 3
+        elif i > 87 and i < 91:
+            i = i - 23
+        elif i > 64 and i < 88:
+            i = i + 3
+        i = chr(i)
+        word.append(i)
+    ciphertext = (''.join(word))
     return ciphertext
-
 
 
 def decrypt_caesar(ciphertext):
@@ -39,19 +38,19 @@ def decrypt_caesar(ciphertext):
     >>> decrypt_caesar("")
     ''
     """
-    # PUT YOUR CODE HERE
-    word0=[]
+    # my code
+    word0 = []
     for i in ciphertext:
-        i=ord(i)
-        if i>64 and i<68:
-            i=i+23
-        elif i>96 and i<100:
-            i+=23
-        elif i>99 and i<123:
-            i=i-3
-        elif i>67 and i<91:
-            i-=3
-        i=chr(i)
+        i = ord(i)
+        if i > 64 and i < 68:
+            i = i + 23
+        elif i > 96 and i < 100:
+            i += 23
+        elif i > 99 and i < 123:
+            i = i - 3
+        elif i > 67 and i < 91:
+            i -= 3
+        i = chr(i)
         word0.append(i)
-    plaintext=(''.join(word0))
+    plaintext = (''.join(word0))
     return plaintext
